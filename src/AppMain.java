7e8a7d4ad139ccdbf6c2ab9c56ccbba3c68c5970
Fabian Schmauder <fabian.schmauder@gmail.com>
2021-01-19 17:11:46 +0100

    add java types

--- src/AppMain.java
+++ src/AppMain.java
@@ -1,5 +1,33 @@
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("Hallo");
+
+        // first String variable
+        //// |0|1|0|0|...... <- welcomeText
+        String welcomeText = "Hello Jan! Nice weather";
+        System.out.println(welcomeText);
+
+        // first boolean
+        // |0| <- niceWeather
+        boolean niceWeather = true;
+        System.out.println(niceWeather);
+
+        // first integer
+        // |0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|1|0|0 < counter
+        int counter = 4;
+        System.out.println(counter);
+
+        // first double (floating point number)
+        double percent = 0.3;
+        System.out.println(percent);
+
+        // add operation
+        int counterPlus5 = counter + 5 ;
+
+        // concat string
+        String output = "Counter result " + counterPlus5;
+        System.out.println(output);
+
+        // mod
+        System.out.println(70 % 60 );
     }
 }
